46e7de6e575c007a1d93edb394fbd67afec102ad
Trung Tran <trung@kiuglobal.com>
2018-07-22 00:46:45 +0700

    added sql orm

--- source.py
+++ source.py
@@ -1,7 +1,10 @@
 from flask import Flask as F
 from flask import request, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = F(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'posgresql://flask:flask@localhost/flask_db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def front_page():
