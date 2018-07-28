d2134b26053a04f29541cbda7e7a0e607bcea8ef
Trung Tran <trung@kiuglobal.com>
2018-07-28 18:24:54 +0700

    config2

--- base/__init__.py
+++ base/__init__.py
@@ -3,7 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from socket import gethostname
 
 app = F(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flask:flask@localhost/flask_db'
+if 'liveconsole' not in gethostname():
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flask:flask@localhost/flask_db'
+else:
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="trung8358573",
+    password="flaskflask",
+    hostname="trung8358573.mysql.pythonanywhere-services.com",
+    databasename="trung8358573$flask")
+    app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+    app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.config['SECRET_KEY'] = 'hard to guess string'
 db = SQLAlchemy(app)
