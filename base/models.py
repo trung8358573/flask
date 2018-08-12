66579eb9bb716cb31df5480902af8095aab0e6f2
Trung Tran <trung@kiuglobal.com>
2018-08-13 01:27:33 +0700

    removed flask-bcrypt

--- base/models.py
+++ base/models.py
@@ -1,7 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from base import db
-from flask_bcrypt import Bcrypt
 
 class User(db.Model):
     __tablename__ = 'users'
